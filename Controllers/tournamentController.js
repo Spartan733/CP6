d399fd0925bc46e3a626be5576758badadff1b5e
Spartan733 <gabrielbois2003@gmail.com>
2026-08-20 15:22:58 +0200

    End US9

--- Controllers/tournamentController.js
+++ Controllers/tournamentController.js
@@ -35,4 +35,56 @@ const createTournament = async (requestAnimationFrame, res) => {
     }
 }
 
-module.exports = { tournament }
+//US9
+const updateTournament = async (req, res) => {
+    try {
+        const tournamentId = req.params.id
+
+        const {
+            name,
+            game,
+            date,
+            rules
+        } = req.body
+
+        const tournament = await this.tournament.findById(tournamentId)
+
+        if(!tournament){
+            return res.status(404).json({message: 'Tournament not find'})
+        }
+
+        if(tournament.createBy.toString() !==req.user._id.toString()){
+            return res.status(403).json({message: 'You do not have the necessary permission to update this tournament'})
+        }
+
+        if(name){
+            tournament.name = name
+        }
+
+        if(game){
+            tournament.game = game
+        }
+
+        if(date){
+            const newDate = new Date(date)
+
+            if(isNaN(tournamentDate.getTime())){
+                return res.status(400).json({message: 'Tournament date is incorrect'})
+            }
+            tournament.date = newDate
+        }
+
+        if(rules){
+            tournament.rules = rules
+        }
+
+        await tournament.save()
+
+        res.status(200).json({message: 'Tournament updateting with succes'})
+
+    } catch (err) {
+        res.status(500).json({message: 'Error during updating the tournament'})
+    }
+}
+
+module.exports = { tournament, updateTournament }
